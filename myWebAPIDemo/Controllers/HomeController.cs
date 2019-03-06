80c9c83a42e9d6e38dbe11b2f106c98a9be3fb0f
Kathleen West <kathleen.elizabeth.west@gmail.com>
2019-03-05 21:52:10 -0500

    cleaned up project

---
+++ myWebAPIDemo/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace myWebAPIDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Hello World";
+            
+            return View();
+        }
+    }
+}
---
+++ myWebAPIDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="myWebAPIDemo.WebApiApplication" Language="C#" %>
